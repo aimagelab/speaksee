a6b89ce50ad1bc200e3974995201af154d85e6cb
Marcella Cornia <marcella.cornia@unimore.it>
2018-05-22 17:02:30 +0200

    Added COCODataset and TabularDataset

--- speaksee/data/dataset.py
+++ speaksee/data/dataset.py
@@ -1,7 +1,9 @@
 import os
 import json
+import numpy as np
 from collections import defaultdict
 from .example import Example
+from pycocotools.coco import COCO
 
 
 class Dataset(object):
@@ -61,12 +63,12 @@ class PairedDataset(Dataset):
         return len(self.examples)
 
 
-class Flickr(PairedDataset):
-    def __init__(self, img_root, ann_file, image_field, text_field):
+class FlickrDataset(PairedDataset):
+    def __init__(self, image_field, text_field, img_root, ann_file):
         dataset = json.load(open(ann_file, 'r'))['images']
         self.train_examples, self.val_examples, self.test_examples = self.get_samples(dataset, img_root)
         examples = self.train_examples + self.val_examples + self.test_examples
-        super(Flickr, self).__init__(examples, image_field, text_field)
+        super(FlickrDataset, self).__init__(examples, image_field, text_field)
 
     @property
     def splits(self):
@@ -93,6 +95,137 @@ class Flickr(PairedDataset):
                 elif d['split'] == 'val':
                     val_samples.append(example)
                 elif d['split'] == 'test':
+                    test_samples.append(example)
+
+        return train_samples, val_samples, test_samples
+
+
+class COCODataset(PairedDataset):
+    def __init__(self, image_field, text_field, img_root, ann_root, id_root=None, use_restval=False):
+        roots = {}
+        roots['train'] = {
+            'img': os.path.join(img_root, 'train2014'),
+            'cap': os.path.join(ann_root, 'captions_train2014.json')
+        }
+        roots['val'] = {
+            'img': os.path.join(img_root, 'val2014'),
+            'cap': os.path.join(ann_root, 'captions_val2014.json')
+        }
+        roots['test'] = {
+            'img': os.path.join(img_root, 'val2014'),
+            'cap': os.path.join(ann_root, 'captions_val2014.json')
+        }
+        roots['trainrestval'] = {
+            'img': (roots['train']['img'], roots['val']['img']),
+            'cap': (roots['train']['cap'], roots['val']['cap'])
+        }
+
+        if id_root is not None:
+            ids = {}
+            ids['train'] = np.load(os.path.join(id_root, 'coco_train_ids.npy'))
+            ids['val'] = np.load(os.path.join(id_root, 'coco_dev_ids.npy'))
+            ids['test'] = np.load(os.path.join(id_root, 'coco_test_ids.npy'))
+            ids['trainrestval'] = (
+                ids['train'],
+                np.load(os.path.join(id_root, 'coco_restval_ids.npy')))
+
+            if use_restval:
+                roots['train'] = roots['trainrestval']
+                ids['train'] = ids['trainrestval']
+        else:
+            ids = None
+
+        self.train_examples, self.val_examples, self.test_examples = self.get_samples(roots, ids)
+        examples = self.train_examples + self.val_examples + self.test_examples
+        super(COCODataset, self).__init__(examples, image_field, text_field)
+
+    @property
+    def splits(self):
+        train_split = PairedDataset(self.train_examples, self.image_field, self.text_field)
+        val_split = PairedDataset(self.val_examples, self.image_field, self.text_field)
+        test_split = PairedDataset(self.test_examples, self.image_field, self.text_field)
+        return train_split, val_split, test_split
+
+    @classmethod
+    def get_samples(cls, roots, ids_dataset=None):
+        train_samples = []
+        val_samples = []
+        test_samples = []
+
+        for split in ['train', 'val', 'test']:
+            if isinstance(roots[split]['cap'], tuple):
+                coco_dataset = (COCO(roots[split]['cap'][0]), COCO(roots[split]['cap'][1]))
+                root = roots[split]['img']
+            else:
+                coco_dataset = (COCO(roots[split]['cap']),)
+                root = (roots[split]['img'],)
+
+            if ids_dataset is None:
+                ids = list(coco_dataset.anns.keys())
+            else:
+                ids = ids_dataset[split]
+
+            if isinstance(ids, tuple):
+                bp = len(ids[0])
+                ids = list(ids[0]) + list(ids[1])
+            else:
+                bp = len(ids)
+
+            for index in range(len(ids)):
+                if index < bp:
+                    coco = coco_dataset[0]
+                    img_root = root[0]
+                else:
+                    coco = coco_dataset[1]
+                    img_root = root[1]
+
+                ann_id = ids[index]
+                caption = coco.anns[ann_id]['caption']
+                img_id = coco.anns[ann_id]['image_id']
+                filename = coco.loadImgs(img_id)[0]['file_name']
+
+                example = Example.fromdict({'image': os.path.join(img_root, filename), 'text': caption})
+
+                if split == 'train':
+                    train_samples.append(example)
+                elif split == 'val':
+                    val_samples.append(example)
+                elif split == 'test':
+                    test_samples.append(example)
+
+        return train_samples, val_samples, test_samples
+
+
+class TabularDataset(PairedDataset):
+    def __init__(self, img_root, ann_file_root, image_field, text_field):
+        self.train_examples, self.val_examples, self.test_examples = self.get_samples(ann_file_root, img_root)
+        examples = self.train_examples + self.val_examples + self.test_examples
+        super(TabularDataset, self).__init__(examples, image_field, text_field)
+
+    @property
+    def splits(self):
+        train_split = PairedDataset(self.train_examples, self.image_field, self.text_field)
+        val_split = PairedDataset(self.val_examples, self.image_field, self.text_field)
+        test_split = PairedDataset(self.test_examples, self.image_field, self.text_field)
+        return train_split, val_split, test_split
+
+    @classmethod
+    def get_samples(cls, ann_file_root, img_root):
+        train_samples = []
+        val_samples = []
+        test_samples = []
+
+        for split in ['train', 'val', 'test']:
+            captions = [f.strip() for f in open(os.path.join(ann_file_root, '%s_caps.txt' % split), 'r').readlines()]
+            filenames = [f.strip() for f in open(os.path.join(ann_file_root, '%s_ims.txt' % split), 'r').readlines()]
+            for caption, filename in zip(captions, filenames):
+                example = Example.fromdict({'image': os.path.join(img_root, filename), 'text': caption})
+
+                if split == 'train':
+                    train_samples.append(example)
+                elif split == 'val':
+                    val_samples.append(example)
+                elif split == 'test':
                     test_samples.append(example)
 
         return train_samples, val_samples, test_samples
