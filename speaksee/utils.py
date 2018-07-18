5b7131850c13eb2464807916fcf00782b972cb8a
Lorenzo Baraldi <baraldi.lorenzo@gmail.com>
2018-07-18 15:48:39 +0200

    Added nostdout context manager

--- speaksee/utils.py
+++ speaksee/utils.py
@@ -1,4 +1,15 @@
 import requests
+import contextlib, sys
+
+class DummyFile(object):
+    def write(self, x): pass
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = DummyFile()
+    yield
+    sys.stdout = save_stdout
 
 
 def reporthook(t):
