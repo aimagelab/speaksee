b537fea1a015db50b20e6b9584118888be9fea78
Lorenzo Baraldi <baraldi.lorenzo@gmail.com>
2018-08-25 12:41:49 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='speaksee',
-      version='0.1',
-      description='Captioning and cross-modal retrieval algorithms',
+      version='0.0.dev1',
+      description='PyTorch utilities and models for Visual-Semantic tasks',
       url='http://github.com/aimagelab/speaksee',
       author='Lorenzo Baraldi, Marcella Cornia',
       author_email='lorenzo.baraldi@unimore.it',
-      license='MIT',
       packages=find_packages(exclude=('test', )),
+      package_data={'speaksee': ['evaluation/stanford-corenlp-3.4.1.jar']},
+      include_package_data=True,
       install_requires=[
           'torch>=0.4.0',
           'torchvision',
@@ -18,5 +19,10 @@ setup(name='speaksee',
           'pycocotools',
           'matplotlib',
           'pathos',
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
       ],
       zip_safe=False)
